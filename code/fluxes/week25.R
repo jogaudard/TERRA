4ca6341f9a5a6f74d1aa9c1af43019e863791f1f
jogaudard <joseph.gaudard@pm.me>
2024-06-12 12:08:21 +0200

    full description of the steps to clean flux data

--- code/fluxes/week25.R
+++ code/fluxes/week25.R
@@ -4,7 +4,23 @@
 # download the data from OSF using data dataDownloader
 
 # read them, eventually select columns, rename coloumns (depends on the logger settings)
+# read also the field record with the time of each measurements and the metadata
 
 # merge data from both loggers?
 
 # use fluxible to calculate fluxes
+
+# use flux_match to match the field record and the concentration data
+
+
+# fux_fitting to fit a model to the concentration over time and calculate a slope
+
+
+# flux_quality and flux_plot to check the quality and see if we need to modify anything
+
+
+# flux_calc to calculate the fluxes
+
+
+# once the clean dataset is there, do not forget to upload it in the clean_data folder on OSF
+# (we avoid doing this automatically because we do not want to take the risk to overwrite the data on OSF in case we messed up something)
