7f61ae17b02cd9e74cd4fce70bddc2a534b81c06
jogaudard <joseph.gaudard@pm.me>
2024-06-12 10:05:11 +0200

    example of cleaning script

---
+++ code/fluxes/week25.R
@@ -0,0 +1,10 @@
+# I am not sure if we want to have one file per campaign or do everything with the same arguments
+# Since the conditions might change I feel that one file per campaing can be smart
+
+# download the data from OSF using data dataDownloader
+
+# read them, eventually select columns, rename coloumns (depends on the logger settings)
+
+# merge data from both loggers?
+
+# use fluxible to calculate fluxes
