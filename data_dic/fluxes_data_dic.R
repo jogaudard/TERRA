9a0c579c2c7161dbda325c442a696945afa7bd11
jogaudard <joseph.gaudard@pm.me>
2024-06-24 15:28:38 +0200

    make data dictionary does not work and I do not know why

--- data_dic/fluxes_data_dic.R
+++ data_dic/fluxes_data_dic.R
@@ -1,7 +1,19 @@
 library(dataDocumentation)
+library(tidyverse)
 # get_started(path = "data_dic")
 
-data_dic_fluxes <- make_data_dictionary(data = biomass,
+description_table <- read_csv("data_dic/description_table.csv")
+fluxes_CO2 <- read_csv("clean_data/fluxes_CO2.csv")
+fluxes_CH4 <- read_csv("clean_data/fluxes_CH4.csv")
+
+fluxes <- bind_rows(fluxes_CO2, fluxes_CH4)
+
+data_dic_fluxes <- make_data_dictionary(data = fluxes_CO2,
                                  description_table = description_table,
-                                 table_ID = "biomass",
-                                 keep_table_ID = FALSE)
+                                 table_ID = "CO2",
+                                 keep_table_ID = TRUE)
+
+
+
+
+data_dic_fluxes
