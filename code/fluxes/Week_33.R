c728e9214004cfcfd80fe9d08947c22aa6012c6b
VinBelde <beldevincent@gmail.com>
2024-09-20 15:40:02 +0200

    removed data complete plot

--- code/fluxes/Week_33.R
+++ code/fluxes/Week_33.R
@@ -53,15 +53,6 @@ conc_df <- left_join(CO2_CH4, PAR_temp)
 
 str(conc_df)
 
-# just some graph to check that the data are complete
-
-conc_df |>
-  pivot_longer(cols = c(CH4, CO2, PAR_in_chamber, PAR_out, T_in_chamber, T_out), names_to = "measurement") |>
-  ggplot(aes(datetime, value)) +
-  geom_point() +
-  facet_grid(measurement~., scales = "free") +
-  scale_x_datetime(date_breaks = "5 hour", minor_breaks = "1 hour")
-
 # use fluxible to calculate fluxes
 
 # use flux_match to match the field record and the concentration data
@@ -102,7 +93,7 @@ slopes_co2_33 <- flux_quality(slopes_co2_33, fit_type = "exp")
 slopes_ch4_33 <- flux_quality(slopes_ch4_33, fit_type = "exp", ambient_conc = 2000)
 
 flux_plot(slopes_co2_33, f_plotname = "week33_co2", f_ylim_upper = 600, output = "pdfpages")
-flux_plot(slopes_ch4_33, f_plotname = "week33_ch4", f_ylim_lower = 1970, f_ylim_upper = 2010, y_text_position = 2000, output = "pdfpages")
+flux_plot(slopes_ch4_33, f_plotname = "week33_ch4", f_ylim_lower = 1970, f_ylim_upper = 2045, y_text_position = 2000, output = "pdfpages")
 
 
 # flux_calc to calculate the fluxes
@@ -229,12 +220,3 @@ fluxes_CH4_33 <- fluxes_33 |>
   ) |>
   arrange(f_start) |>
   select(!f_fluxID) # we remove flux_ID because it will be repeated with the next batch of data
-
-
-# need to upload to OSF: 1 file for CO2, 1 for CH4, continuous adding (function for that?)
-
-# only for week 25
-
-write_csv(fluxes_CO2, "clean_data/fluxes_CO2.csv")
-
-write_csv(fluxes_CH4, "clean_data/fluxes_CH4.csv")
